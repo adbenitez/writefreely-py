5a33854ab0d70eac82600f76715835b06c89b42f
adbenitez <asieldbenitez@gmail.com>
2021-09-09 14:46:30 -0400

    move requirements to folder

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
+"""Setup package installation"""
+
+import os
+
 import setuptools
+
+
+def load_requirements(path: str) -> list:
+    """Load requirements from the given relative path."""
+    with open(path, encoding="utf-8") as file:
+        requirements = []
+        for line in file.read().split("\n"):
+            if line.startswith("-r"):
+                dirname = os.path.dirname(path)
+                filename = line.split(maxsplit=1)[1]
+                requirements.extend(load_requirements(os.path.join(dirname, filename)))
+            elif line and not line.startswith("#"):
+                requirements.append(line.replace("==", ">="))
+        return requirements
+
 
 if __name__ == "__main__":
     with open("README.md") as file:
         long_desc = file.read()
-    with open("requirements.txt") as file:
-        install_requires = [
-            line.replace("==", ">=")
-            for line in file.read().split("\n")
-            if line and not line.startswith(("#", "-"))
-        ]
-    with open("requirements-test.txt") as file:
-        test_deps = [
-            line.replace("==", ">=")
-            for line in file.read().split("\n")
-            if line and not line.startswith(("#", "-"))
-        ]
 
     setuptools.setup(
         name="writefreely-py",
@@ -37,6 +44,9 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3",
         ],
         python_requires=">=3.5",
-        install_requires=install_requires,
-        extras_require={"test": test_deps},
+        install_requires=load_requirements("requirements/requirements.txt"),
+        extras_require={
+            "test": load_requirements("requirements/requirements-test.txt"),
+            "dev": load_requirements("requirements/requirements-dev.txt"),
+        },
     )
